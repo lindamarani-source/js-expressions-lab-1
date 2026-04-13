56b90199ffb2d966172c20605e3c9b6222f66a92
lindamarani-source <linda.marani@student.moringaschool.com>
2026-04-13 19:19:24 +0300

    Commit

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-//! Start by creating the variables for the data recorded
-//* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
+var fahrenheitTemps = [32, 70, 80, 72, 68, 75, 82, 65, 77, 78, 73, 79, 71, 74, 76];
+var celsiusTemps = [25, 18, 15, 28, 20, 23, 30, 22, 24, 21, 27, 19, 26, 17, 29];
 
+function convertToCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
 
-//! Start the calculation of the total temperatures
-//* Then apply the conversion to calculate the total in the other unit of measurement
-//* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
+function convertToFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
 
-//! Start the calculation of the average temperatures
-//* Call the variables: avg_temperature_in_fahrenheit and avg_temperature_in_celsius
+var allTempsInFahrenheit = fahrenheitTemps.concat(celsiusTemps.map(convertToFahrenheit));
+var allTempsInCelsius = celsiusTemps.concat(fahrenheitTemps.map(convertToCelsius));
 
-//! Console.log the results for your own inspection if you'd like
+var tot_temperature_in_fahrenheit = allTempsInFahrenheit.reduce((sum, temp) => sum + temp, 0);
+var tot_temperature_in_celsius = allTempsInCelsius.reduce((sum, temp) => sum + temp, 0);
 
-//! After creating the four variables mentioned above, uncomment the following lines
-//* This way you can export them to the test file, this is essential for the tests to work
+var avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / allTempsInFahrenheit.length;
+var avg_temperature_in_celsius = tot_temperature_in_celsius / allTempsInCelsius.length;
 
 module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
+  tot_temperature_in_fahrenheit,
+  tot_temperature_in_celsius,
+  avg_temperature_in_fahrenheit,
+  avg_temperature_in_celsius
 };
